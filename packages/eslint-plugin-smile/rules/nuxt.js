fa37041cd4fa1342b8c878c86ff14470e06b1188
Tony CABAYE <tony.cabaye@smile.fr>
2023-11-23 12:27:57 +0100

    fix: lint and format

--- packages/eslint-plugin-smile/rules/nuxt.js
+++ packages/eslint-plugin-smile/rules/nuxt.js
@@ -1,15 +1,14 @@
-  // @see https://github.com/nuxt/eslint-plugin-nuxt/tree/master
-  const nuxtRules = {
-    'nuxt/no-timing-in-fetch-data': 'error',
-    'nuxt/require-func-head': 'error',
-    'nuxt/no-env-in-context': 'error',
-    'nuxt/no-env-in-hooks': 'error',
-    'nuxt/no-globals-in-created': 'error',
-    'nuxt/no-this-in-fetch-data': 'error',
-    'nuxt/no-cjs-in-config': 'error',
-  };
-  
-  export default {
-    ...nuxtRules,
-  };
-  
+// @see https://github.com/nuxt/eslint-plugin-nuxt/tree/master
+const nuxtRules = {
+  'nuxt/no-cjs-in-config': 'error',
+  'nuxt/no-env-in-context': 'error',
+  'nuxt/no-env-in-hooks': 'error',
+  'nuxt/no-globals-in-created': 'error',
+  'nuxt/no-this-in-fetch-data': 'error',
+  'nuxt/no-timing-in-fetch-data': 'error',
+  'nuxt/require-func-head': 'error',
+};
+
+export default {
+  ...nuxtRules,
+};
